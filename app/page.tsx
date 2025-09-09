e4865193229149caad9b968063e0c745f45dced3
Amar Bodonguud <64552518+ramamaa@users.noreply.github.com>
2025-09-09 22:31:00 +0800

    homework

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,23 @@
 import { MovieCard, NowPlaying, Header } from "@/components/my";
+import { ArrowRight } from "lucide-react";
 export default function Home() {
   return (
     <div>
       <Header />
       <NowPlaying />
+      <div className="flex justify-between items-center mx-20 mt-13 mb-8">
+        <span className="text-foreground text-2xl leading-8 font-semibold">
+          Upcoming{" "}
+        </span>
+        <button className="text-foreground text-sm flex items-center gap-2 px-4 py-2 border rounded-md font-medium">
+          See more
+          <ArrowRight className="text-primary w-4 h-4" />
+        </button>
+      </div>
       <MovieCard
         title="Dark Knight"
         image="https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_FMjpg_UX1000_.jpg"
-        score={9.9}
-      ></MovieCard>
+        score={9.9}></MovieCard>
     </div>
   );
 }
--- components/my/Moviecard.tsx
+++ components/my/Moviecard.tsx
@@ -17,7 +17,7 @@ type MovieCardProps = {
 export const MovieCard = ({ title, image, score }: MovieCardProps) => {
   return (
     <div>
-      <Card className="w-[230px] bg-secondary p-0 overflow-hidden gap-1 flex flex-col items-start text-foreground  ">
+      <Card className="w-[230px] bg-secondary p-0 overflow-hidden gap-1 flex flex-col items-start text-foreground  mx-20">
         <CardContent className="p-0">
           <Image src={image} alt="" width={230} height={340} unoptimized />
           <CardFooter className="flex flex-col items-start self-stretch p-2 ">
@@ -27,8 +27,7 @@ export const MovieCard = ({ title, image, score }: MovieCardProps) => {
                 width="16"
                 height="15"
                 viewBox="0 0 16 15"
-                fill="none"
-              >
+                fill="none">
                 <path
                   d="M7.99967 1.33337L10.0597 5.50671L14.6663 6.18004L11.333 9.42671L12.1197 14.0134L7.99967 11.8467L3.87967 14.0134L4.66634 9.42671L1.33301 6.18004L5.93967 5.50671L7.99967 1.33337Z"
                   fill="#FDE047"
